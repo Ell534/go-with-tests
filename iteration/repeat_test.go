59906657b4bd62a76aedb481d3f01c2edb38ee6d
Ell534 <elliot94b@gmail.com>
2024-01-08 17:16:45 +0000

    add and test repeat function with specified repeat value, add example function in test

--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -1,12 +1,37 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
-	expected := "aaaaa"
+	t.Run("repeat character default number of times", func(t *testing.T) {
+		repeated := Repeat("a", 0)
+		expected := "aaaaa"
 
-	if repeated != expected {
-		t.Errorf("expected %q but got %q", expected, repeated)
+		if repeated != expected {
+			t.Errorf("expected %q but got %q", expected, repeated)
+		}
+	})
+	t.Run("repeat character specified number of times", func(t *testing.T) {
+		repeated := Repeat("b", 10)
+		expected := "bbbbbbbbbb"
+
+		if repeated != expected {
+			t.Errorf("expected %q but got %q", expected, repeated)
+		}
+	})
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 0)
 	}
+}
+
+func ExampleRepeat() {
+	repeated := Repeat("c", 3)
+	fmt.Println(repeated)
+	// output: ccc
 }
